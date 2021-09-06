4e2eb1fdb4d12c29b8bc13879746c52162d8ca17
tranduchuy682 <55192155+tranduchuy682@users.noreply.github.com>
2021-09-06 13:10:49 +0700

    Accuracy 96%

---
+++ resnet.py
@@ -0,0 +1,143 @@
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+import tensorflow.keras as keras
+from tensorflow.keras import models
+from tensorflow.keras import layers
+from tensorflow.keras import optimizers
+import tensorflow as tf
+from keras.utils import np_utils
+from keras.models import load_model
+from keras.datasets import cifar10
+from keras.preprocessing import image
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import cv2
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+
+conv_base = ResNet50(weights='imagenet', include_top=False, input_shape=(256, 256, 3))
+
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+x_train, y_train = shuffle(x_train, y_train, random_state=10)
+
+x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size = 0.1, stratify = y_train)
+
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+x_val = x_val / 255.0
+y_train = np_utils.to_categorical(y_train, 10)
+y_test = np_utils.to_categorical(y_test, 10)
+y_val = np_utils.to_categorical(y_val, 10)
+
+print(x_train.shape)
+print(y_train.shape)
+print(x_val.shape)
+print(y_val.shape)
+print(x_test.shape)
+print(y_test.shape)
+
+
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+width_shift = 3/32
+height_shift = 3/32
+flip = True
+EPOCHS = 50
+
+datagen = ImageDataGenerator(
+    horizontal_flip=flip,
+    width_shift_range=width_shift,
+    height_shift_range=height_shift,
+    rotation_range=15,
+    )
+datagen.fit(x_train)
+
+# it = datagen.flow(x_train, y_train, shuffle=False)
+# batch_images, batch_labels = next(it)
+
+model = models.Sequential()
+model.add(layers.UpSampling2D((2,2)))
+model.add(layers.UpSampling2D((2,2)))
+model.add(layers.UpSampling2D((2,2)))
+model.add(conv_base)
+model.add(layers.Flatten())
+model.add(layers.BatchNormalization())
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dropout(0.5))
+model.add(layers.BatchNormalization())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dropout(0.5))
+model.add(layers.BatchNormalization())
+model.add(layers.Dense(10, activation='softmax'))
+model.compile(optimizer=optimizers.RMSprop(lr=2e-5), loss='binary_crossentropy', metrics=['acc'])
+
+datagen.fit(x_train)
+
+history = model.fit(
+    datagen.flow(x_train, y_train, batch_size=64),
+    epochs=EPOCHS,
+    validation_data=(x_val, y_val))
+loss, acc = model.evaluate(x_test,  y_test)
+
+def plot_accuracy_loss_chart(history):
+    epochs = [i for i in range(EPOCHS)]
+    fig , ax = plt.subplots(1,2)
+    train_acc = history.history['acc']
+    train_loss = history.history['loss']
+    val_acc = history.history['val_acc']
+    val_loss = history.history['val_loss']
+    fig.set_size_inches(20,10)
+    ax[0].plot(epochs , train_acc , 'go-' , label = 'Training Accuracy')
+    ax[0].plot(epochs , val_acc , 'ro-' , label = 'Validation Accuracy')
+    ax[0].set_title('Training & Validation Accuracy')
+    ax[0].legend()
+    ax[0].set_xlabel("Epochs")
+    ax[0].set_ylabel("Accuracy")
+
+    ax[1].plot(epochs , train_loss , 'g-o' , label = 'Training Loss')
+    ax[1].plot(epochs , val_loss , 'r-o' , label = 'Validation Loss')
+    ax[1].set_title('Training & Validation Loss')
+    ax[1].legend()
+    ax[1].set_xlabel("Epochs")
+    ax[1].set_ylabel("Training & Validation Loss")
+    plt.show()
+plot_accuracy_loss_chart(history)
+
+
+results = model.evaluate(x_test, y_test)
+
+print("Loss of test model is  ", results[0])
+print("Accuracy of test model is ", results[1]*100, "%")
+
+
+results = model.evaluate(x_val, y_val)
+
+print("Loss of validation model is ", results[0])
+print("Accuracy of validation model is ", results[1]*100, "%")
+
+results = model.evaluate(x_train, y_train)
+
+print("Loss of train model  is ", results[0])
+print("Accuracy of train model is ", results[1]*100, "%")
+
+from sklearn.metrics import classification_report
+
+predictions = model.predict(x_test)
+predictions = np.argmax(predictions,axis=1)
+
+y_test = np.argmax(y_test,axis=1)
+
+print(classification_report(
+    y_test, 
+    predictions, 
+    target_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']))
+
+from sklearn.metrics import confusion_matrix
+import pandas as pd
+cm = confusion_matrix(y_test, predictions)
+cm = pd.DataFrame(cm, 
+                  index = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'], 
+                  columns = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'])
